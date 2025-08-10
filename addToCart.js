da07663248c87d639e41fef852d4a94d87e61612
Aaryan Thakur <princeaaryan06@gmail.com>
2025-08-10 16:24:58 +0545

    update with cart function

--- addToCart.js
+++ addToCart.js
@@ -1,4 +1,22 @@
+import { getCartProductFromLS } from "./getCartProducts.js";
+
 export const addToCart=(envet,id,stock)=>{
+
+    let arrLocalStorageProduct=getCartProductFromLS();
+
+    const currentProdElem=document.querySelector(`#card${id}`);
+
+    let quantity= currentProdElem.querySelector(".productQuantity").innerText;
+
+    let price=currentProdElem.querySelector(".productPrice").innerText;
+
+    // console.log(quantity, price);
+    
+    price=price.replace("£","");
+    price=price * quantity;
+
+    arrLocalStorageProduct.push({id, quantity, price})
+    localStorage.setItem("cartProductLS", JSON.stringify(arrLocalStorageProduct));
     
 
-}
+};
